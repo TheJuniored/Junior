0e0edb59d29ea619eec427049984aab0ba53b2ba
TheJuniored <francois.halbout@epita.fr>
2022-12-03 16:37:57 +0100

    ok

--- sudoku-solver-main/sudoku-solver-main/sudoku_solver/filetab.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILETAB_H
-#define FILETAB_H
-
-//we define all our functions which are in .c in the .h
-
-void tab_to_file(int tab[], char *filename);
-void file_to_tab(int tab[], char *filename);
-
-#endif
